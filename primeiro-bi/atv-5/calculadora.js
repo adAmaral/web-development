773123510e62c679e9fc34c91dd6dae84fc9e2c7
Adriel <adriel.alegrette@gmail.com>
2026-03-29 20:49:07 -0300

    atv-5

---
+++ primeiro-bi/atv-5/calculadora.js
@@ -0,0 +1,6 @@
+const valorConta = 100
+const percentualGorjeta = 10
+var valorGorjeta = valorConta * percentualGorjeta / 100
+var valorTotal = valorConta + valorGorjeta
+
+console.log("Valor da conta: R$ " + valorConta + "\nPercentual da gorjeta: " + percentualGorjeta + "%\nValor da gorjeta: R$ " + valorGorjeta + "\nValor total a pagar: R$ " + valorTotal)
